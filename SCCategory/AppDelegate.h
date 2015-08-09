ed1642e53987fd03c5abfb746c1b733e5c168ea5
SeanChense <chenyu380@gmail.com>
2015-08-09 17:52:29 +0800

    init

---
+++ SCCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCCategory
+//
+//  Created by SeanChense on 15/8/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCCategory
+//
+//  Created by SeanChense on 15/8/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
